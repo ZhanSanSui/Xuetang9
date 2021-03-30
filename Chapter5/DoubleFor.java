6f5c58f3a508ee8f43ce6bfa3efbb2bdf9012bf4
ZhanSansui <1062189799@qq.com>
2021-03-30 20:04:27 +0800

    3.30chapter5 20.04更新提交

---
+++ Chapter5/DoubleFor.java
@@ -0,0 +1,130 @@
+package Chapter5;
+
+/**
+ * 双循环
+ * @author：Wen
+ * @version：1.0
+ * @date：2021/3/29 21:40
+ * @copyright 老九学堂
+ * @remark TODO
+ */
+public class DoubleFor {
+    public static void main(String[] args) {
+        //行、列 - 双循环
+        //外层循环控制行，内层循环控制列
+
+//        //外层循环控制行：控制行数、控制换行
+//        for (int i = 0; i < 5; i++){
+//            //内层循环控制列：控制列数、控制列打印的符号！
+//            for (int j = 0; j < 5; j++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+
+        //打印图形-题1
+        //每行里面先打印空格，再打印*
+//        for (int i = 0; i < 4; i++){
+//            //一般用i来表示j的最大值
+//            for (int j = 0; j <= 2 - i; j++){
+//                System.out.print(" ");
+//            }
+//            //打印完空格之后，再打印*
+//            for (int j = 0; j <= 2 * i; j++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+//
+//        for (int i = 0; i < 3; i++){
+//            //一般用i来表示j的最大值
+//            for (int j = 0; j <= i; j++){
+//                System.out.print(" ");
+//            }
+//            //打印完空格之后，再打印*
+//            for (int j = 0; j <= 4 - 2 * i; j++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+
+        //打印图形-题2
+//        char ch = 'A';
+//        for (int i = 0; i < 4; i++){
+//            //一般用i来表示j的最大值
+//            for (int j = 0; j <= 2 - i; j++){
+//                System.out.print(" ");
+//            }
+//            //打印完空格之后，再打印A
+//            for (int j = 0; j <= 2 * i; j++){
+//                System.out.print((char)ch);
+//            }
+//            ch++;
+//            System.out.println();
+//        }
+//
+//        for (int i = 0; i < 3; i++){
+//            //一般用i来表示j的最大值
+//            for (int j = 0; j <= i; j++){
+//                System.out.print(" ");
+//            }
+//            //打印完空格之后，再打印A
+//            for (int j = 0; j <= 4 - 2 * i; j++){
+//                System.out.print((char)ch);
+//            }
+//            ch++;
+//            System.out.println();
+//        }
+
+        //打印图形-题3
+//        for (int i = 0; i < 4; i++){
+//            //一般用i来表示j的最大值
+//            for (int j = 0; j <= 2 - i; j++){
+//                System.out.print(" ");
+//            }
+//            //打印完空格之后，再打印*
+//            for (int j = 0; j <= 2 * i; j++){
+//                if (j == 0 || j == 2 * i){ //j是最大值或最小值打印*
+//                    System.out.print("*");
+//                }else {
+//                    System.out.print(" ");
+//                }
+//            }
+//            System.out.println();
+//        }
+//
+//        for (int i = 0; i < 3; i++){
+//            //一般用i来表示j的最大值
+//            for (int j = 0; j <= i; j++){
+//                System.out.print(" ");
+//            }
+//            //打印完空格之后，再打印*
+//            for (int j = 0; j <= 4 - 2 * i; j++){
+//                if (j == 0 || j == 4 - 2 * i){ //j是最大值或最小值打印*
+//                    System.out.print("*");
+//                }else {
+//                    System.out.print(" ");
+//                }
+//            }
+//            System.out.println();
+//        }
+
+        //还需要再进阶->打印幂次方
+//        int a = 1;
+//        for (int i = 0; i < 9; i++){
+//            //一般用i来表示j的最大值
+//            for (int j = 0; j <= 16 - 2 * i; j++){
+//                System.out.print(" ");
+//            }
+//            //打印完空格之后，再打印*
+//            for (int j = 0; j <= 2 * i ; j++){
+//                System.out.printf("%2d",a);
+//            }
+//            System.out.println();
+//        }
+
+
+
+
+    }
+}
---
+++ Chapter5/MoneyTotal.java
@@ -0,0 +1,33 @@
+package Chapter5;
+
+import java.util.Scanner;
+
+/**
+ * 循环输入5个玩家的消费，统计消费额低于500的玩家数量
+ * 如果消费大于等于500，则跳过；小于500则计数器+1
+ * @author：Wen
+ * @version：1.0
+ * @date：2021/3/29 21:24
+ * @copyright 老九学堂
+ * @remark TODO
+ */
+public class MoneyTotal {
+    public static void main(String[] args) {
+        //1.循环录入5个玩家的消费金额
+        //2.录入后立刻判断是否小于500
+        //3.累加小于500的总次数
+        Scanner input = new Scanner(System.in);
+        int money = Integer.MAX_VALUE; //玩家消费额,赋一个不可能的值，方便后面的判断
+        int total = 0; //消费额低于500的玩家数量
+        for (int i = 0; i < 5; i++){
+            System.out.println("请输入第" + (i + 1) + "位玩家的消费额：");
+            money = input.nextInt();
+            if (money >= 500){
+                continue;
+            }else {
+                total++;
+            }
+        }
+        System.out.println("消费额低于500的玩家数量：" + total);
+    }
+}
